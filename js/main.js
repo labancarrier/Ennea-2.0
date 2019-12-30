89a09f1cad828cb3f90b104eabcf27339abbc814
MyMac <mymac@macbook-pro-15.lan>
2019-12-30 16:52:43 +0700

    mailchimp

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@
     
     var cfg = {
         scrollDuration : 800, // smoothscroll duration
-        mailChimpURL   : 'https://facebook.us8.list-manage.com/subscribe/post?u=cdb7b577e41181934ed6a6a44&amp;id=e6957d85dc'   // mailchimp url
+        mailChimpURL   : 'https://enneasphere.us20.list-manage.com/subscribe/post?u=f82ab8636d72f9b31a4965812&amp;id=26841764e4'   // mailchimp url
     },
 
     $WIN = $(window);
